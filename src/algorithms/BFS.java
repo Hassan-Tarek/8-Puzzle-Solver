fac434a91c83d4a706ec7d0344a4bbab79498d00
Hassan Tarek <hassanelashry415@gmail.com>
2023-10-22 22:08:53 +0200

    Add BFS algorithm implementation

--- src/algorithms/BFS.java
+++ src/algorithms/BFS.java
@@ -3,6 +3,12 @@ package algorithms;
 import agent.Agent;
 import agent.Node;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 public class BFS extends Agent {
     public BFS(Node startNode, Node goalNode) {
         super(startNode, goalNode);
@@ -10,6 +16,29 @@ public class BFS extends Agent {
 
     @Override
     public void search() {
+        Queue<Node> frontier = new LinkedList<>();
+        Set<Node> visited = new HashSet<>();
 
+        frontier.add(this.getStartNode());
+
+        while (!frontier.isEmpty()) {
+            Node currentNode = frontier.poll();
+
+            if (!visited.contains(currentNode)) {
+                visited.add(currentNode);
+
+                if (isGoal(currentNode)) {
+                    setSolution(constructSolution(currentNode));
+                    return;
+                }
+
+                List<Node> successors = currentNode.expand();
+                for (Node successor : successors) {
+                    if (!visited.contains(successor)) {
+                        frontier.add(successor);
+                    }
+                }
+            }
+        }
     }
 }
