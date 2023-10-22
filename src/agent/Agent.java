87c88dba2c21a1e23b88ba8a0b081ee85fbb3ff7
Hassan Tarek <hassanelashry415@gmail.com>
2023-10-22 11:34:07 +0200

    Complete Agent and AgentFactory implementation

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -1,4 +1,46 @@
 package agent;
 
+import java.util.List;
+
 public class Agent {
+    private Node startNode;
+    private Node goalNode;
+    private List<Node> solution;
+
+    public Agent(Node startNode, Node goalNode) {
+        this.startNode = startNode;
+        this.goalNode = goalNode;
+    }
+
+    public Node getStartNode() {
+        return startNode;
+    }
+
+    public void setStartNode(Node startNode) {
+        this.startNode = startNode;
+    }
+
+    public Node getGoalNode() {
+        return goalNode;
+    }
+
+    public void setGoalNode(Node goalNode) {
+        this.goalNode = goalNode;
+    }
+
+    public List<Node> getSolution() {
+        return solution;
+    }
+
+    public void setSolution(List<Node> solution) {
+        this.solution = solution;
+    }
+
+    public boolean isGoal(Node node) {
+        return node.equals(goalNode);
+    }
+
+    public int getDepth() {
+        return goalNode.getDepth();
+    }
 }
--- src/agent/AgentFactory.java
+++ src/agent/AgentFactory.java
@@ -1,9 +1,16 @@
 package agent;
 
+import algorithms.AStar;
+import algorithms.BFS;
+import algorithms.DFS;
 import algorithms.ISearch;
 
 public class AgentFactory {
     public static ISearch createAgent(AgentType agentType) {
-        return null;
+        return switch (agentType) {
+            case DFS -> new DFS();
+            case BFS -> new BFS();
+            case A_STAR -> new AStar();
+        };
     }
 }
