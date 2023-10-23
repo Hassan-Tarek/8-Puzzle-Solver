1f9110b1479a5173caab7833563b4b238d3ee8c2
Hassan Tarek <hassanelashry415@gmail.com>
2023-10-23 21:58:43 +0200

    Fix A_STAR instantiation in AgentType enum

--- src/agent/AgentType.java
+++ src/agent/AgentType.java
@@ -3,13 +3,15 @@ package agent;
 import algorithms.AStar;
 import algorithms.BFS;
 import algorithms.DFS;
+import algorithms.HeuristicType;
 
 import java.util.function.BiFunction;
 
 public enum AgentType {
     DEPTH_FIRST_SEARCH(DFS::new),
     BREADTH_FIRST_SEARCH(BFS::new),
-    A_STAR(AStar::new);
+    A_STAR_EUCLIDEAN((startNode, goalNode) -> new AStar(HeuristicType.EUCLIDEAN_DISTANCE, startNode, goalNode)),
+    A_STAR_MANHATTAN((startNode, goalNode) -> new AStar(HeuristicType.MANHATTAN_DISTANCE, startNode, goalNode));
 
     private final BiFunction<Node, Node, Agent> agentSupplier;
 
